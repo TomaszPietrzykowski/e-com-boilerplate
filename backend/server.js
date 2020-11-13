309f9aa4df902a407a00f68573c2cc163642efc1
TomaszPietrzykowski <pietrzykowski77@gmail.com>
2020-11-13 22:16:00 +0100

    env setup, node updated

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,23 @@
-const express = require('express');
-const products = require('./data/products');
+import express from 'express';
+import dotenv from 'dotenv';
+import products from './data/products.js';
+
+dotenv.config();
 
 const app = express();
 
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
   res.send('API is running...');
 });
-app.get('/products', (req, res) => {
+app.get('/api/products', (req, res) => {
   res.json(products);
 });
-
-app.listen(5000, console.log('Server started on port 5000'));
+app.get('/api/products/:id', (req, res) => {
+  const product = products.find((p) => p._id === req.params.id);
+  res.json(product);
+});
+const PORT = process.env.PORT;
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`)
+);
