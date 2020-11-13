713e6f51ba42715108fe6636891b3f9a6b9c21a9
TomaszPietrzykowski <pietrzykowski77@gmail.com>
2020-11-13 22:40:49 +0100

    db hooked in, mongoose added

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import products from './data/products.js';
+import mongoose from 'mongoose';
 
 dotenv.config();
 
@@ -16,6 +17,21 @@ app.get('/api/products/:id', (req, res) => {
   const product = products.find((p) => p._id === req.params.id);
   res.json(product);
 });
+
+const DB = process.env.DB_URI;
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then((con) =>
+    console.log(
+      `MongoDB connected.... \n database user: ${con.connections[0].user}`
+    )
+  );
+
 const PORT = process.env.PORT;
 app.listen(
   PORT,
