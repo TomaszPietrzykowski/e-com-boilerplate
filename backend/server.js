a0b2f27238479b89a6d71b3400ea2d4017f926ad
TomaszPietrzykowski <pietrzykowski77@gmail.com>
2020-11-16 14:13:32 +0100

    error handling middleware added

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import colors from 'colors';
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 dotenv.config();
 connectDB();
@@ -13,6 +14,10 @@ app.get('/api', (req, res) => {
 });
 
 app.use('/api/products', productRoutes);
+
+app.use(notFound);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(
