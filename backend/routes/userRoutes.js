b6652eb51285d4811c8b5e5bd132d996d39e43d4
TomaszPietrzykowski <pietrzykowski77@gmail.com>
2020-11-19 20:34:41 +0100

    auth middleware added

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,13 @@
 import express from 'express';
 const router = express.Router();
-import { authUser } from '../controllers/userController.js';
+import { authUser, getUserProfile } from '../controllers/userController.js';
+import { protect } from '../middleware/authMiddleware.js';
 
 // @description: authenticate user & get token
 // @route: POST /api/users/login
 // @access: Public
 router.post('/login', authUser);
+
+router.route('/profile').get(protect, getUserProfile);
 
 export default router;
