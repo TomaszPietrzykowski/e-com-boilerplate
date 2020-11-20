39974220fd7d5c0843e2db54bb0390e7ed84ef5c
TomaszPietrzykowski <pietrzykowski77@gmail.com>
2020-11-20 20:40:37 +0100

    create user route and controller added

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,13 +1,14 @@
 import express from 'express';
 const router = express.Router();
-import { authUser, getUserProfile } from '../controllers/userController.js';
+import {
+  authUser,
+  getUserProfile,
+  registerUser,
+} from '../controllers/userController.js';
 import { protect } from '../middleware/authMiddleware.js';
 
-// @description: authenticate user & get token
-// @route: POST /api/users/login
-// @access: Public
+router.route('/').post(registerUser);
 router.post('/login', authUser);
-
 router.route('/profile').get(protect, getUserProfile);
 
 export default router;
