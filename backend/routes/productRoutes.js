d5955e53d46fbe9bd3f216e5092aac262faa2deb
TomaszPietrzykowski <pietrzykowski77@gmail.com>
2021-01-13 12:43:49 +0100

    delete product added to admin

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,16 +3,12 @@ const router = express.Router();
 import {
   getProducts,
   getProductById,
+  deleteProduct,
 } from '../controllers/productController.js';
+import { protect, admin } from '../middleware/authMiddleware.js';
 
-// @description: Fetch all products
-// @route: GET /api/products
-// @access: Public
+
 router.route('/').get(getProducts);
-
-// @description: Fetch single product
-// @route: GET /api/products/:id
-// @access: Public
-router.route('/:id').get(getProductById);
+router.route('/:id').get(getProductById).delete(protect, admin, deleteProduct);
 
 export default router;
