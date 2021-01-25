4d373e19152a4a41dc0bc205971ec3f939f3749b
TomaszPietrzykowski <pietrzykowski77@gmail.com>
2021-01-25 11:42:42 +0100

    pagination added to admin

--- frontend/src/screens/ProductListScreen.js
+++ frontend/src/screens/ProductListScreen.js
@@ -4,15 +4,18 @@ import { Button, Table, Col, Row } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import Message from '../components/Message';
 import Loader from '../components/Loader';
+import Paginate from '../components/Paginate';
 import { listProducts, deleteProduct, createProduct } from "../actions/productActions"
 import { PRODUCT_CREATE_RESET } from "../constants/productConstants"
 
 const ProductListScreen = ({ history, match }) => {
     
+    const pageNumber = match.params.pageNumber || 1
+
     const dispatch = useDispatch()
 
     const productList = useSelector(state => state.productList)
-    const { loading, error, products } = productList
+    const { loading, error, products, page, pages } = productList
 
     const productDelete = useSelector(state => state.productDelete)
     const { loading:loadingDelete, error:errorDelete, success:successDelete } = productDelete
@@ -31,9 +34,9 @@ const ProductListScreen = ({ history, match }) => {
     if (successCreate) {
         history.push(`/admin/product/${createdProduct._id}/edit`)
     } else {
-        dispatch(listProducts())
+        dispatch(listProducts('', pageNumber))
     }
-    }, [dispatch, history, userInfo, successDelete, successCreate, createdProduct])
+    }, [dispatch, history, userInfo, successDelete, successCreate, createdProduct, pageNumber])
 
     const deleteHandler = (id) => {
         if(window.confirm("Are you sure?")) {
@@ -62,6 +65,7 @@ const ProductListScreen = ({ history, match }) => {
         {loadingCreate && <Loader />}
         {errorCreate && <Message variant="danger">{errorCreate}</Message>}
             {loading ? <Loader /> : error ? <Message variant="danger">{error}</Message> : (
+                <>
                 <Table striped bordered hover responsive className="table-sm">
                     <thead>
                         <tr>
@@ -95,6 +99,8 @@ const ProductListScreen = ({ history, match }) => {
                         ))}
                     </tbody>
                 </Table>
+                <Paginate pages={pages} page={page} isAdmin={true} />
+                </>
             )}
         </>
     )
